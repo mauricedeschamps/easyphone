6add4c6c0c8654bf198bf1546c77e6a9217bc2be
mauricedeschamps <mementomorikatsu@gmail.com>
2025-05-14 15:31:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'easyphone-v1';
+const ASSETS_TO_CACHE = [
+  '/easyphone/',
+  '/easyphone/index.html',
+  '/easyphone/icons/icon-192x192.png',
+  '/easyphone/icons/icon-512x512.png',
+  '/easyphone/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
